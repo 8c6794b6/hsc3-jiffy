0060812a3779aec93232ec4754334018a86f899a
8c6794b6 <8c6794b6@gmail.com>
2019-02-27 15:31:19 +0900

    Use "foldl'" for dump graph
    Use "foldl'" instead of "foldr'" in sample graph of "dump-graph"
    executable.

--- exec/dump-graph.hs
+++ exec/dump-graph.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Data.Foldable (foldr')
+-- base
+import Data.Foldable (foldl')
 
 -- deepseq
 import Control.DeepSeq
@@ -14,12 +15,12 @@ g07 :: Int -> UGen
 g07 n = do
   d <- share (dust KR 2)
   let o0 = sinOsc AR (mce [440,441]) 0 * decay d 1
-      f _ b = do
+      f b _ = do
         let cg = coinGate 0.012 d
         dt <- share (lag (tExpRand 1e-5 0.4 cg) 1.8)
         b' <- share b
         clip2 (b' + hpf (delayN b' 0.4 dt * 0.8) 20) 1
-      o1 = foldr' f o0 [0 .. n]
+      o1 = foldl' f o0 [0 .. n]
   out 0 (o1 * 0.5)
 
 main :: IO ()
