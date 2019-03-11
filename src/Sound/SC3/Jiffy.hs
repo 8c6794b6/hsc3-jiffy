1641910a1199a5dc71617e662d08fe56e07cb291
8c6794b6 <8c6794b6@gmail.com>
2019-03-11 17:30:02 +0900

    Update haddock comment of Sound.SC3.Jiffy
    Modify haddock comments in "Sound.SC3.Jiffy" module.

--- src/Sound/SC3/Jiffy.hs
+++ src/Sound/SC3/Jiffy.hs
@@ -1,14 +1,28 @@
 -- | Replacement module for "Sound.SC3".
 --
--- This module re-exports modules re-exported from "Sound.SC3", except
--- for UGen related modules and functions which are re-exported from
+-- This module re-exports modules exported from "Sound.SC3", except for
+-- UGen related modules and functions which are re-exported from
 -- hsc3-jiffy package.
+--
+-- Some of the functions exported from "Sound.SC3.Server.Monad" are
+-- using 'Sound.SC3.UGen.Type.UGen' type defined in hsc3 package, which
+-- could not be reused with 'UGen' type defined in hsc3-jiffy
+-- package. This module hides functions using the hsc3
+-- 'Sound.SC3.UGen.Type.UGen' and re-exports rest of the functions.
 
-module Sound.SC3.Jiffy (module All) where
+module Sound.SC3.Jiffy
+  ( -- * Re-exported modules
+    module Sound.SC3.Common
+  , module Sound.SC3.Jiffy.Compat
+  , module Sound.SC3.UGen.Jiffy
+
+    -- * Re-exported functions from Sound.SC3.Server.Monad
+  , module Sound.SC3.Server.Monad
+  ) where
 
 -- hsc3
-import Sound.SC3.Common as All
-import Sound.SC3.Server.Monad as All hiding
+import Sound.SC3.Common
+import Sound.SC3.Server.Monad hiding
   ( Synthdef(..), synthdef
   , defaultSynthdef, defaultSampler
   , synthdefGraph, synthdefParam, synthdef_to_graphdef, synthdefData
@@ -17,5 +31,5 @@ import Sound.SC3.Server.Monad as All hiding
   , d_recv )
 
 -- Internal
-import Sound.SC3.Jiffy.Compat as All
-import Sound.SC3.UGen.Jiffy as All
+import Sound.SC3.Jiffy.Compat
+import Sound.SC3.UGen.Jiffy
