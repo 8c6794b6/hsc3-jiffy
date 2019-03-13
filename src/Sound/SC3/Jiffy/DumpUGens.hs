c57ccaee7ca531e8639c28d6028a5c7cb0caf404
8c6794b6 <8c6794b6@gmail.com>
2019-03-13 15:29:42 +0900

    Update format of dumpUGens
    Modify the format of "dumpUGens" to match the output from
    "SynthDef.dumpUGens" in sclang.

--- src/Sound/SC3/Jiffy/DumpUGens.hs
+++ src/Sound/SC3/Jiffy/DumpUGens.hs
@@ -1,6 +1,9 @@
--- | Dump UGens in graph.
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+-- |
+-- A module containing 'DumpUGens' type class, which is for dumping
+-- the ugen contents of synthdef graphs.
+--
 module Sound.SC3.Jiffy.DumpUGens
   ( DumpUGens(..)
   ) where
@@ -8,15 +11,21 @@ module Sound.SC3.Jiffy.DumpUGens
 -- base
 import GHC.IO.Handle.FD (stdout)
 
+--- array
+import Data.Array.IArray (Array, (!), listArray)
+import Data.Array.Unboxed (UArray)
+
 -- bytestring
 import qualified Data.ByteString.Builder as B
-import Data.ByteString.Lazy.Char8 (pack, unpack)
+import Data.ByteString.Lazy.Char8 (unpack)
 
 -- hsc3
+import Sound.SC3.Common.Math.Operator (unaryName, binaryName)
 import Sound.SC3.Server.Graphdef (Graphdef(..))
 import Sound.SC3.Server.Graphdef.Graph (graph_to_graphdef)
 import Sound.SC3.UGen.Graph (U_Graph(..))
 import qualified Sound.SC3 as SC3
+import qualified Sound.SC3.Server.Graphdef as SC3SG
 import qualified Sound.SC3.UGen.Graph as SC3UG
 
 -- Internal
@@ -30,8 +39,10 @@ import Sound.SC3.UGen.Jiffy.Builder
 class DumpUGens a where
   dumpUGenString :: a -> String
   dumpUGens :: a -> IO ()
-  dumpUGens = putStr . dumpUGenString
+  dumpUGens = putStrLn . dumpUGenString
 
+-- | The dumped format is similar to the format used in
+-- "@SynthDef.dumpUGens@" method from /sclang/.
 instance DumpUGens Graphdef where
   dumpUGenString = unpack . B.toLazyByteString . dump_graphdef
   dumpUGens = B.hPutBuilder stdout . dump_graphdef
@@ -63,24 +74,78 @@ instance DumpUGens SC3.Synthdef where
 ugen_to_graphdef' :: String -> SC3.UGen -> Graphdef
 ugen_to_graphdef' n u = graph_to_graphdef n (SC3UG.ugen_to_graph u)
 
+
+-- -----------------------------------------------------------------------
+--
+-- Guts
+--
+-- -----------------------------------------------------------------------
+
+-- | Array for cnstants.
+type CA = UArray Int SC3.Sample
+
+-- | Array for UGen tuples.
+type UA = Array Int SC3SG.UGen
+
 dump_graphdef :: Graphdef -> B.Builder
 dump_graphdef gd =
   unlines'
-    [ "name: " <> B.lazyByteString (pack (show (graphdef_name gd)))
-    , "--- constants ---"
-    , printsWithIndex (graphdef_constants gd)
-    , "--- controls ---"
-    , printsWithIndex (graphdef_controls gd)
-    , "--- ugens ---"
-    , printsWithIndex (graphdef_ugens gd) ]
+    [ B.byteString (graphdef_name gd)
+    , let cs = graphdef_constants gd
+          carr = listArray (0,length cs) cs
+          us = graphdef_ugens gd
+          uarr = listArray (0,length us) us
+      in  printsWithIndex carr uarr us
+    , "" ]
+
+printsWithIndex :: CA -> UA -> [SC3SG.UGen] -> B.Builder
+printsWithIndex ca ua ugens =
+  case ugens of
+    [] -> "No output"
+    _  -> unlines' (zipWith (printOne ca ua) [0..] ugens)
+
+printOne :: CA -> UA -> Int -> SC3SG.UGen -> B.Builder
+printOne ca ua i ug@(_name,rate,inputs,_,_special) =
+  brackets [pName i Nothing ug, pRate rate, pInputs inputs]
+  where
+    pName j mb_port (n,_,_,_,s) =
+      B.intDec j <> "_" <>
+      case n of
+        "UnaryOpUGen"  -> B.string7 (unaryName s)
+        "BinaryOpUGen" -> B.string7 (binaryName s)
+        "Control"      | Just p <- mb_port -> pControl n p
+        "TrigControl"  | Just p <- mb_port -> pControl n p
+        "AudioControl" | Just p <- mb_port -> pControl n p
+        _              -> B.byteString n
+    pControl n p = B.byteString n <> "[" <> B.intDec p <> "]"
+    pRate r =
+      case toEnum r of
+        SC3.IR -> "scalar"
+        SC3.KR -> "control"
+        SC3.AR -> "audio"
+        SC3.DR -> "demand"
+    pInputs is =
+      case is of
+        [] -> "nil"
+        _  -> brackets (map pInput is)
+    pInput (SC3SG.Input t k) =
+      case t of
+        -1 -> B.doubleDec (ca ! k)
+        _  -> pName t (Just k) (ua ! t)
 
 unlines' :: [B.Builder] -> B.Builder
-unlines' = foldr (\a b -> a <> "\n" <> b) mempty
+unlines' = intercalate "\n"
+{-# INLINE unlines' #-}
 
-printsWithIndex :: Show a => [a] -> B.Builder
-printsWithIndex xs =
-  case xs of
-    [] -> "None."
-    _  -> let f x y = B.intDec x <> ": " <>
-                      B.lazyByteString (pack (show y))
-          in  unlines' (zipWith f [(0::Int) ..] xs)
+brackets :: [B.Builder] -> B.Builder
+brackets xs = "[ " <> intercalate ", " xs <> " ]"
+
+intercalate :: B.Builder -> [B.Builder] -> B.Builder
+intercalate b = go
+  where
+    go xs =
+      case xs of
+        []    -> mempty
+        x:[]  -> x
+        x:xs' -> x <> b <> go xs'
+{-# INLINE intercalate #-}
